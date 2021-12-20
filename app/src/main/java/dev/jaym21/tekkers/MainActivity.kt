14ed5be10fb2986d1089b40e6da990d376df2745
jaym21 <jaynmistry21@gmail.com>
2021-12-20 11:22:55 +0530

    connected bottom navigation to host fragment

--- app/src/main/java/dev/jaym21/tekkers/MainActivity.kt
+++ app/src/main/java/dev/jaym21/tekkers/MainActivity.kt
@@ -2,10 +2,27 @@ package dev.jaym21.tekkers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dev.jaym21.tekkers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavView: BottomNavigationView = binding!!.bottomNavigationView
+
+        val navController = findNavController(R.id.navHostFragment)
+        bottomNavView.setupWithNavController(navController)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
