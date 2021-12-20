db9ddea3445bf6588eb5f3f130bb0508da912d7a
jaym21 <jaynmistry21@gmail.com>
2021-12-20 10:15:19 +0530

    added fixtures fragment

---
+++ app/src/main/java/dev/jaym21/tekkers/FixturesFragment.kt
@@ -0,0 +1,27 @@
+package dev.jaym21.tekkers
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.tekkers.databinding.FragmentFixturesBinding
+
+class FixturesFragment : Fragment() {
+
+    private var binding: FragmentFixturesBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentFixturesBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
