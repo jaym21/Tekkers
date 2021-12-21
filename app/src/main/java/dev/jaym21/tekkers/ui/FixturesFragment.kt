b62714c1062f0c8246015ece2719d54979b715cb
jaym21 <jaynmistry21@gmail.com>
2021-12-21 11:02:16 +0530

    implemented displaying fixture dates in recycler view

--- app/src/main/java/dev/jaym21/tekkers/ui/FixturesFragment.kt
+++ app/src/main/java/dev/jaym21/tekkers/ui/FixturesFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import dev.jaym21.tekkers.adapters.FixtureDatesRVAdapter
 import dev.jaym21.tekkers.databinding.FragmentFixturesBinding
+import dev.jaym21.tekkers.utils.DateUtils
 
 class FixturesFragment : Fragment() {
 
     private var binding: FragmentFixturesBinding? = null
+    private lateinit var fixtureDatesAdapter: FixtureDatesRVAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +22,24 @@ class FixturesFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentFixturesBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //getting fixture dates timestamp array
+        val fixtureDates = DateUtils().getTimestampsOfDatesForFixtures()
+
+        fixtureDatesAdapter = FixtureDatesRVAdapter(fixtureDates.toList())
+
+        setUpFixtureDatesRV()
+    }
+
+    private fun setUpFixtureDatesRV() {
+        binding?.rvDates?.apply {
+            adapter = fixtureDatesAdapter
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
     }
 
     override fun onDestroy() {
